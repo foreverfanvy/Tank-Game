1e09b26ab6d3ba23db21c73b8023a24d0275b079
yaoyao <yaojingxi027@gmail.com>
2025-04-27 15:45:06 +0800

    坦克大战1.5.1版本，敌人发出子弹个数的设计功能，初始默认为1,可以进行修改

--- src/Tank_Res/EnemyTank.java
+++ src/Tank_Res/EnemyTank.java
@@ -58,11 +58,30 @@ public class EnemyTank extends Tank implements Runnable {
                     }
                     break;
             }
-
             setDirection((int) (Math.random() * 4));//随机设置一个方向[0,4)利用int取整
             //退出线程的条件设计！！！（并发程序的关键）
             if (this.isLive == false) break;//被击中->isLive==false就退出循环
-
+            // 判断shots的size个数来进行子弹数量的创建与限制
+            Shot s = null;
+            if(shots.size() == 1  && isLive) {//这里的size设置的内容是敌人子弹个数的最大值，一般设置一个不然会出现很多子弹导致很乱
+                switch (getDirection()) {
+                    case 0:
+                        s= new Shot(getX()+20,getY(),0);
+                        break;
+                    case 1:
+                        s = new Shot(getX()+60,getY()+20,1);
+                        break;
+                    case 2:
+                        s = new Shot(getX()+20,getY()+60,2);
+                        break;
+                    case 3:
+                        s = new Shot(getX(),getY()+20,3);
+                        break;
+                }
+                //
+                shots.add(s);
+                new Thread(s).start();
+            }
         }
     }
 }
